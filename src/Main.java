00590f9dadeb42fd9d033cc3053896c06bd1de88
Pavel Shilov <pavel.s.shilov@gmail.com>
2021-10-31 10:47:51 +0300

    update Main class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int currentAccount = 2_000_000_000;
-        int transfer = 500_000;
+        int transfer = 500_000_000;
         int summary = currentAccount + transfer;
         System.out.println(summary);
     }
